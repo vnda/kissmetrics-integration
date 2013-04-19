23c956fb033a6287af5c3e6c4084c374dea06506
Guilherme Schneider <guigs81@gmail.com>
2013-04-19 18:34:47 -0300

    Aplicação Rails 4.0 criada com comando "rails new . -d postgresql -S -J"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaltyThicket9488::Application.config.secret_key_base = 'fab4970d42149215bde1c3fdea57b75078f3aec0a3200675e18ceb55964c4438591d18516294ba8cb012ebe6166e5906ce3714ea1de138f32753d303a4470b29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SaltyThicket9488::Application.config.session_store :encrypted_cookie_store, key: '_salty-thicket-9488_session'
